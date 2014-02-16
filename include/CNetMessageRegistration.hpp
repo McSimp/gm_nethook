4e266fc7d15548ee62ca737745796a43b7219d2e
McSimp <will@mcsi.mp>
2014-02-17 01:24:35 +1100

    Improve svc_Print, create messages properly

--- include/CNetMessageRegistration.hpp
+++ include/CNetMessageRegistration.hpp
@@ -13,8 +13,6 @@ typedef bool (__thiscall* tMsgWriteFunc) (INetMessage* msg, bf_write& buffer);
 
 class CNetMessageRegistration
 {
-    void** m_vtable;
-
     FuncPtr<tMsgWriteFunc> m_writeOriginalFunc;
     FuncPtr<tMsgWriteFunc> m_writeHookFunc;
     CSimpleDetour<tMsgWriteFunc> m_writeDetour;
@@ -22,6 +20,9 @@ class CNetMessageRegistration
     FuncPtr<tLuaSetupFunc> m_luaSetupFunc;
 
     std::string m_msgName;
+
+protected:
+    void** m_vtable;
 
 public:
     static std::map<std::string, CNetMessageRegistration&> RegisteredMessages;
@@ -46,6 +47,20 @@ public:
     CMessageClassRegistration(const std::string& msgName)
         : CNetMessageRegistration(msgName, MakeFuncPtr<tMsgWriteFunc>(&T::WriteHook), MakeFuncPtr<tLuaSetupFunc>(&T::InitializeLua))
     {}
+
+    T* CreateNewMessage()
+    {
+        if (!IsResolved())
+            return nullptr;
+
+        T* obj = new T();
+
+        // Replace the vtable with the engine's vtable
+        void*** pVtable = reinterpret_cast<void***>(obj);
+        pVtable[0] = m_vtable;
+
+        return obj;
+    }
 };
 
 #endif
--- src/messages/svc_Print.cpp
+++ src/messages/svc_Print.cpp
@@ -3,6 +3,7 @@
 #include "gm/Lua.hpp"
 #include "gm/LuaBindThunk.hpp"
 #include "CHookedNetMessage.hpp"
+#include "strtools.h"
 
 using namespace GarrysMod::Lua;
 
@@ -11,12 +12,13 @@ CMessageClassRegistration<svc_Print> Registration("svc_Print");
 
 class svc_Print : public CHookedNetMessage<svc_Print, Registration>
 {
+public:
     svc_Print() { m_bReliable = false; m_text = nullptr; };
 
-public:
     void* m_pMessageHandler;
     const char* m_text;
-    
+    char m_textBuffer[2048];
+
     static const char* LuaMetaTableName;
     static const int LuaTypeID;
 
@@ -33,8 +35,8 @@ public:
     int SetTextLua(CLuaInterface& Lua)
     {
         Lua.CheckType(2, Type::STRING);
-        // TODO: Need to copy this string
-        m_text = Lua.GetString(2);
+        V_strcpy_safe(m_textBuffer, Lua.GetString(2));
+        m_text = m_textBuffer;
         return 0;
     }
 
@@ -42,9 +44,9 @@ public:
     {
         Lua.CheckType(1, Type::STRING);
         
-        svc_Print* obj = new svc_Print();
-        // TODO: Need to copy this string
-        obj->m_text = Lua.GetString(1);
+        svc_Print* obj = Registration.CreateNewMessage();
+        V_strcpy_safe(obj->m_textBuffer, Lua.GetString(1));
+        obj->m_text = obj->m_textBuffer;
         Lua.PushBoundObject(obj);
 
         return 1;
@@ -58,9 +60,9 @@ public:
         __index.SetMember("SetText", LuaMemberBindThunk<svc_Print, &svc_Print::SetTextLua>);
         mt.SetMember("__index", __index);
 
-        //Lua.SetGlobal("svc_Print", LuaStaticBindThunk<&svc_Print::LuaCreateObject>);
+        Lua.SetGlobal("svc_Print", LuaStaticBindThunk<&svc_Print::LuaCreateObject>);
     }
 };
 
 const char* svc_Print::LuaMetaTableName = "nethook.svc_Print";
-const int svc_Print::LuaTypeID = 205;
+const int svc_Print::LuaTypeID = 200;
